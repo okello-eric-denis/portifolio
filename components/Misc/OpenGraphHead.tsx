a8cbb77196eba9a81d781e3489f227fbc41028b8
Eric <eric@localhost.localdomain>
2024-05-28 12:42:24 +0300

    &apos;

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -2,13 +2,13 @@ import Head from 'next/head'
 const OpenGraphHead = () => (
   <Head>
     <title>Okello Eric | Computer Engineer</title>
-    <meta name="description" content="My personal solace place on web-earth." />
+    <meta name="description" content="My personal solace palace on web-earth." />
     <meta property="og:title" content="Okello Eric | Computer Engineer" />
     <meta property="og:site_name" content="Okello Eric" />
     <meta property="og:url" content="https://okello-eric.netlify.app/" />
     <meta
       property="og:description"
-      content="Ohh you found me?. Howdy! I am Okello Eric. Or you could call me Eric. That works too . . . I am a Computer Engineer"
+      content="I am Okello Eric. Or you could call me Eric. That works too . . . I am a Computer Engineer"
     />
     <meta property="og:type" content="profile" />
     <meta
