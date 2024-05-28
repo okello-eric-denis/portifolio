5eacd5603a85c7f6cd667d311fa5c9a0aa5a14d7
Eric <eric@localhost.localdomain>
2024-05-28 13:04:27 +0300

    i have worked on fixing the description

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -1,9 +1,12 @@
-import Head from 'next/head';
+import Head from 'next/head'
 
 const OpenGraphHead = () => (
   <Head>
     <title>Okello Eric | Computer Engineer</title>
-    <meta name="description" content="My personal solace palace on web-earth." />
+    <meta
+      name="description"
+      content="My personal solace palace on web-earth."
+    />
     <meta property="og:title" content="Okello Eric | Computer Engineer" />
     <meta property="og:site_name" content="Okello Eric Portfolio" />
     <meta property="og:url" content="https://okello-eric.netlify.app/" />
@@ -12,8 +15,11 @@ const OpenGraphHead = () => (
       content="Hello there! I am Okello Eric. Or you could call me Eric. That works too . . . I am a Computer Engineer"
     />
     <meta property="og:type" content="profile" />
-    <meta property="og:image" content="https://okello-eric.netlify.app/photo.jpg" />
+    <meta
+      property="og:image"
+      content="https://okello-eric.netlify.app/photo.jpg"
+    />
   </Head>
-);
+)
 
-export default OpenGraphHead;
+export default OpenGraphHead
