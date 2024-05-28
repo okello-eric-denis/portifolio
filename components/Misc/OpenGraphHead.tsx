ae8d0f47504462c3c7be37b25c5403a2de837a65
Eric <eric@localhost.localdomain>
2024-05-28 12:58:33 +0300

    description

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -1,20 +1,19 @@
-import Head from 'next/head'
+import Head from 'next/head';
+
 const OpenGraphHead = () => (
   <Head>
     <title>Okello Eric | Computer Engineer</title>
     <meta name="description" content="My personal solace palace on web-earth." />
     <meta property="og:title" content="Okello Eric | Computer Engineer" />
-    <meta property="og:site_name" content="Okello Eric" />
+    <meta property="og:site_name" content="Okello Eric Portfolio" />
     <meta property="og:url" content="https://okello-eric.netlify.app/" />
     <meta
       property="og:description"
-      content=" Hello there! I am Okello Eric. Or you could call me Eric. That works too . . . I am a Computer Engineer"
+      content="Hello there! I am Okello Eric. Or you could call me Eric. That works too . . . I am a Computer Engineer"
     />
     <meta property="og:type" content="profile" />
-    <meta
-      property="og:image"
-      content="https://okello-eric.netlify.app/photo.jpg"
-    ></meta>
+    <meta property="og:image" content="https://okello-eric.netlify.app/photo.jpg" />
   </Head>
-)
-export default OpenGraphHead
+);
+
+export default OpenGraphHead;
