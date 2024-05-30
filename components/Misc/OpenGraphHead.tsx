c7ae460fcdb272d14acba5e9816d88a37d5be590
Eric <eric@localhost.localdomain>
2024-05-30 13:06:04 +0300

    second commit

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -24,6 +24,7 @@ const OpenGraphHead = () => {
       />
       <meta property="og:type" content="profile" />
       <meta property="og:image" content={imageSrc} />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
   )
 }
